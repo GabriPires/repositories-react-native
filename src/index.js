b6b0dd3684ed6aadceed4a92fdf5df131989ecf4
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-09 10:48:23 -0300

    Configurando reactotron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import './config/ReactotronConfig';
+
 // import { Container } from './styles';
 
 const styles = StyleSheet.create({
