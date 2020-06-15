14557d688fa279a0fa18ae24913f80293fb80dfc
GabriPires <gabriel.santos.pes@gmail.com>
2020-06-15 10:28:50 -0300

    Exibindo perfil

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Header, Avatar, Name, Bio } from './styles';
 
 export default class User extends Component {
   static propTypes = {
@@ -31,8 +30,19 @@ export default class User extends Component {
   }
 
   render() {
+    const { route } = this.props;
     const { start } = this.state;
 
-    return <View />;
+    const { user } = route.params;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+      </Container>
+    );
   }
 }
